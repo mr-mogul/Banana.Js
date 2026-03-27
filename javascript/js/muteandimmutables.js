07e53a86545726725a25ca778e329ce6722d2e5d
mr-mogul <igbenoghenekarosam@gmail.com>
2026-03-27 14:16:58 +0100

    Mutables and Immuatables

---
+++ javascript/js/muteandimmutables.js
@@ -0,0 +1,13 @@
+//Mutables  (can be changed)
+let fruits = ["mango", "banana"]
+fruits.push("peer")
+fruits[0] = "fish"
+
+//Immutables (cannot be changed)
+let name = "Sophia"
+name[0]= "P"
+
+
+
+console.log(fruits)
+console.log(name)
